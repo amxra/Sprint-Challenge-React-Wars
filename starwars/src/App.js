a62bbffa7ee6bb5464d4947df7f1cbac5eb90595
amxra <amiradediran@gmail.com>
2019-09-13 13:32:53 +0100

    Set state and call Api using useEffect

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+import Characters from "./components/characters";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+    const [info, setInfo] = useState([])
+
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(()=>{
+    axios.get('https://swapi.co/api/people')
+    .then(response => {
+      console.log(response)
+      setInfo(response.data.results)
+    })
+    .catch(err => {
+      console.log('Error! Try Again')
+    })
+  },[])
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Characters characters ={info}/>
     </div>
   );
 }
